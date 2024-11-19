70c60b760bb03134017c8eb8657ac1f4895dca70
brainon24 <gestionbraion24@gmail.com>
2024-11-19 11:41:00 -0500

    fix: catching errors

--- src/app.ts
+++ src/app.ts
@@ -14,45 +14,48 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/twilio-hook', async (req, res) => {
+  try {
+    const twiml = new twilio.twiml.MessagingResponse();
+    const ctx = req.body;
+    const { data: user } = await axios.get(
+      `${process.env.BACKEND_URL}/user/findByPhone/${ctx?.WaId?.substring(
+        2,
+        ctx?.WaId?.length
+      )}`
+    );
+    // if ( user?.role !== 'CLIENT' || user?.role !== 'ALLY' ) return;
 
-  const twiml = new twilio.twiml.MessagingResponse();
-  const ctx = req.body;
-  const { data: user } = await axios.get(
-    `${process.env.BACKEND_URL}/user/findByPhone/${ctx?.WaId?.substring(
-      2,
-      ctx?.WaId?.length
-    )}`
-  );
-  // if ( user?.role !== 'CLIENT' || user?.role !== 'ALLY' ) return;
+    twiml.message(`Gracias ${user?.fullName} por escribirnos. Hemos recibido tu mensaje y lo estamos gestionando. ¡Estamos conectados!`);
+    if (ctx?.MessageType !== "text") {
+      parseSecret()
+      let media = ctx?.MediaContentType0?.split('/')[1]
+      if (media?.includes('ogg')) {
+        media = 'mp3'
+      }
+      const url = await getFile(ctx?.MediaUrl0);
+      const resp = await uploadFile(url, media);
 
-  twiml.message(`Gracias ${user?.fullName} por escribirnos. Hemos recibido tu mensaje y lo estamos gestionando. ¡Estamos conectados!`);
-  if (ctx?.MessageType !== "text") {
-    parseSecret()
-    let media = ctx?.MediaContentType0?.split('/')[1]
-    if (media?.includes('ogg')) {
-      media = 'mp3'
+      const urlParsed = `https://storage.cloud.google.com/${process.env.GCP_BUCKET_NAME}/${resp?.filename}`;
+      await axios.post(`${process.env.BACKEND_URL}/message`, {
+        userId: user?._id,
+        content: ctx?.Body,
+        attachment: urlParsed,
+        attachmentType: media,
+      });
+      res.set('Content-Type', 'text/xml');
+      res.send(twiml.toString());
+      return;
     }
-    const url = await getFile(ctx?.MediaUrl0);
-    const resp = await uploadFile(url, media);
-
-    const urlParsed = `https://storage.cloud.google.com/${process.env.GCP_BUCKET_NAME}/${resp?.filename}`;
     await axios.post(`${process.env.BACKEND_URL}/message`, {
       userId: user?._id,
       content: ctx?.Body,
-      attachment: urlParsed,
-      attachmentType: media,
     });
-    res.set('Content-Type', 'text/xml');
-    res.send(twiml.toString());
-    return;
-  }
-  await axios.post(`${process.env.BACKEND_URL}/message`, {
-    userId: user?._id,
-    content: ctx?.Body,
-  });
 
-  res.set('Content-Type', 'text/xml');
-  res.send(twiml.toString());
+    res.set('Content-Type', 'text/xml');
+    res.send(twiml.toString()); 
+  } catch (error) {
+    console.log({ error })
+  }
 });
 
 const port = process.env.PORT || 3000;
